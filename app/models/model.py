f7acf3b773bf70a6792afa44d0e0ed3e8addb0ab
nicolaspessel <nicolaspessel@gmail.com>
2026-04-01 10:10:06 -0300

    feat: changed models syntax from Column to Mapped and mapped_column

--- app/models/model.py
+++ app/models/model.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()  # uses the declarative base object
 
 class Team(Base):  # inherits fom declarative base
+    """The Mapped and mapped_column are the modern syntax for SQLAlchemy 2.0, allowing the use of python 
+    native types and type hints, while Column is the legacy declaration for all SQLAlchemy systems and versions."""
+
     __tablename__ = "Team X"
 
-    id: Integer = Column(Integer, primary_key=True, autoincrement=True, index=True)  # surrogate key
-    name: String = Column(String, index=True)
-    titles: Integer = Column(Integer, index=True)
-    region: String = Column(String, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)  
+    name: Mapped[str]  # class that represents an ORM mapped attribute in a class
+    titles: Mapped[int]
+    region: Mapped[str]
