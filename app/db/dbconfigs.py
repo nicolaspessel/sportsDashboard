ff08f0a905ce1de98fdde5bfd278515527bcd5d2
nicolaspessel <nicolaspessel@gmail.com>
2026-04-02 21:28:49 -0300

    fix: transferred get_session function to create dependency injection in router

--- app/db/dbconfigs.py
+++ app/db/dbconfigs.py
@@ -6,15 +6,6 @@ from sqlalchemy.orm import sessionmaker
 def create_db_and_tables(Base):
 	Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-	db = Session()
-	try:
-		yield db  # pause and returns the session value
-	finally:
-		db.close()  # once the session is finished, the session is closed
-
-
 load_dotenv()  # imports all the environment variables
 sqlite_url = os.getenv('DATABASE_URL')  # defines the database and dialect object
 
