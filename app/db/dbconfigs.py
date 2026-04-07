f16784b3811abee321d9d5d6ae0836563be977e0
nicolaspessel <nicolaspessel@gmail.com>
2026-04-07 19:18:01 -0300

    fix: realocated get_session w/ yield method to import in router

--- app/db/dbconfigs.py
+++ app/db/dbconfigs.py
@@ -6,10 +6,18 @@ from sqlalchemy.orm import sessionmaker
 def create_db_and_tables(Base):
 	Base.metadata.create_all(bind=engine)
 
+def get_session():
+	session = SessionLocal()  # when calling the factory, a Session object is returned
+	try:
+		yield session  # pause and returns the session value
+	finally:
+		session.close()  # once the session is finished, the session is closed
+
 load_dotenv()  # imports all the environment variables
 sqlite_url = os.getenv('DATABASE_URL')  # defines the database and dialect object
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args,echo=True)  # lazy initialization of the engine
 
-Session = sessionmaker(engine)  # factory (pattern) for Session class instances with engine as default
+# factory for Session class object with engine as defaut
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
