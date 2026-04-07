bdab0e86a470b456fc231f316a28681e94c83245
nicolaspessel <nicolaspessel@gmail.com>
2026-04-07 19:18:58 -0300

    fix: renamed variables for a cleaner and organized code

--- app/services/service.py
+++ app/services/service.py
@@ -5,10 +5,10 @@ def get_team_by_id(team_id: int, session: Session):
     team = TeamRepository(session=session)
     return team.get_team_by_id(team_id)
 
-def create_team(name: str, titles: int, region:str, session: Session):
+def create_new_team(name: str, titles: int, region:str, session: Session):
     new_team = TeamRepository(session=session)
     new_team.create_team(name=name, titles=titles, region=region)
 
-def delete_team(team_id: int, session: Session):
+def remove_item(team_id: int, session: Session):
     team_to_del = TeamRepository(session=session)
     team_to_del.delete_team(team_id=team_id)
