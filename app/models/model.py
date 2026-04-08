3fa6ef1aee3654b8e60d2d7842b6e9f55df225ba
nicolaspessel <nicolaspessel@gmail.com>
2026-04-08 11:17:42 -0300

    renamed class and tablename

--- app/models/model.py
+++ app/models/model.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()  # uses the declarative base object
 
-class Team(Base):  # inherits fom declarative base
+class Teams(Base):  # inherits fom declarative base
     """The Mapped and mapped_column are the modern syntax for SQLAlchemy 2.0, allowing the use of python 
     native types and type hints, while Column is the legacy declaration for all SQLAlchemy systems and versions."""
 
-    __tablename__ = "Team X"
+    __tablename__ = "Teams"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)  
     name: Mapped[str]  # class that represents an ORM mapped attribute in a class
