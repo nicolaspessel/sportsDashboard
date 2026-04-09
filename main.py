0db3434c3661ee9873f5d546e757d3e016f631f7
nicolaspessel <nicolaspessel@gmail.com>
2026-04-08 21:47:25 -0300

    feat: added router configurations w/ FastAPI object and lifespan function to load tables

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-from app.models.model import Team
+from contextlib import asynccontextmanager
+from app.models.model import Teams
 from app.db.dbconfigs import create_db_and_tables
+from app.routers.router import router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    create_db_and_tables()
+    yield
+    # shutdown
 
-t = Team()
-create_db_and_tables()
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router)
 
 @app.get("/")
 async def root():
