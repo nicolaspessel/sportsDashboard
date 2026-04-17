385cb3560587db1ecc3501e432da403fe7a8f810
nicolaspessel <nicolaspessel@gmail.com>
2026-04-17 01:58:16 -0300

    feat: added titles to TeamResponse schema

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -8,6 +8,7 @@ class TeamCreate(TeamBase):
     titles: int = 0
 
 class TeamResponse(TeamBase):
+    titles: int
     id: int
 
     model_config = ConfigDict(from_attributes=True)  # allows Pydantic to read ORM attributes
