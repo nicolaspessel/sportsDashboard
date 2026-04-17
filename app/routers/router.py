3d61feb7bd445147f33480e858a9351f39b20182
nicolaspessel <nicolaspessel@gmail.com>
2026-04-17 00:23:05 -0300

    feat: created patch method and endpoint within repo, service and router flow

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..db.dbconfigs import get_session
-from ..schemas.schemas import TeamResponse, TeamCreate, TeamBase
-from ..services.service import get_team_by_id, create_new_team, remove_item
+from ..schemas.schemas import TeamResponse, TeamCreate, TeamUpdate
+from ..services.service import get_team_by_id, create_new_team, remove_item, patch_team
 
 router = APIRouter()
 
@@ -14,6 +14,16 @@ def get_team(team_id: int, session: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail="Item not found")  # terminates function and sends HTTP error
 
     return team
+
+@router.patch("/teams/{team_id}", response_model=TeamResponse)
+def update_team(team_id: int, team_update: TeamUpdate, session: Session = Depends(get_session)):
+    team = patch_team(team_id=team_id, team_update=team_update, session=session)
+
+    if not team:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    return team
+    
 
 @router.post("/teams/", status_code=201)
 def create_team(team: TeamCreate, session: Session = Depends(get_session)):
--- app/services/service.py
+++ app/services/service.py
@@ -1,9 +1,14 @@
 from sqlalchemy.orm import Session
 from ..repositories.repo import TeamRepository
+from ..schemas.schemas import TeamUpdate
 
 def get_team_by_id(team_id: int, session: Session):
     team = TeamRepository(session=session)
-    return team.get_team_by_id(team_id)
+    return team.get_team_by_id(team_id=team_id)
+
+def patch_team(team_id: int, team_update: TeamUpdate, session: Session):
+    team_to_updt = TeamRepository(session=session)
+    return team_to_updt.update_team(team_id=team_id, team_update=team_update)
 
 def create_new_team(name: str, titles: int, region:str, session: Session):
     new_team = TeamRepository(session=session)
