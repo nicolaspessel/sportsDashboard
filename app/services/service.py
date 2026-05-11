9331958611af5e996d6d355904fa0930f7eb3d94
nicolaspessel <nicolaspessel@gmail.com>
2026-05-11 11:14:18 -0300

    feat: created patch_stadiums and implemented a try/except for KeyError in get_all_teams

--- app/services/service.py
+++ app/services/service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from ..repositories.repo import TeamRepository, StadiumRepository
-from ..schemas.schemas import TeamUpdate
+from ..schemas.schemas import TeamUpdate, StadiumUpdate
 from ..clients.client import ESPNClient
 from ..exceptions import TeamNotFoundError
 
@@ -10,20 +10,21 @@ def get_team_by_id(team_id: int, session: Session):
 
 
 def get_all_teams(session: Session):
-    repo = TeamRepository(session=session)
-    db_teams = repo.get_teams()
+    tm_repo = TeamRepository(session=session)
+    stdm_repo = StadiumRepository(session=session)
+    db_teams = tm_repo.get_teams()
 
     if not db_teams:
-        client = ESPNClient(base_url="https://sports.core.api.espn.com/v3/sports/basketball/leagues/nba")
+        client = ESPNClient(base_url="https://sports.core.api.espn.com/v2/sports/basketball/leagues/nba")
         
         page = 1
         refs = []
 
         while True:
-            request = client.get_teams({'pageIndex': page})
-            refs.extend(request['items'])  # appends all the items without excluding
+            request = client.get_teams(params={'page': page})
+            refs.extend(request.get('items'))  # appends all the items without excluding
 
-            if page == request['pageCount']:
+            if page >= request.get('pageCount'):
                 break
 
             page += 1  # changes the page to execute more requests
@@ -32,17 +33,27 @@ def get_all_teams(session: Session):
             team_url = ref['$ref'].partition('nba')[2]
             team_data = client.get_team_by_ref(team_url=team_url)
 
-            name = team_data['displayName']
-            abbreviation = team_data['abbreviation']
-            is_active = team_data['isActive']
-            region = team_data['location']
+            tm_name = team_data.get('displayName')
+            abbreviation = team_data.get('abbreviation')
+            is_active = team_data.get('isActive')
+            tm_region = team_data.get('location')
 
-            repo.create_team(name=name, abbreviation=abbreviation, is_active=is_active, 
-                             titles=0, region=region)
+            try:
+                stdm_name = team_data['venue']['fullName']
+                stdm_location = team_data['venue']['address']['city']
+                stdm_location += f',  {team_data['venue']['address']['state']}'
+            except KeyError:
+                stdm_name = 'Unknown'
+                stdm_location = 'Unknown'
+
+            new_team = tm_repo.create_team(name=tm_name, abbreviation=abbreviation, is_active=is_active, 
+                                            titles=0, region=tm_region)
+            session.commit()
+
+            stdm_repo.create_stadium(name=stdm_name, location=stdm_location, team_id=new_team.id    )
             session.commit()
         
-
-    db_teams = repo.get_teams()
+    db_teams = tm_repo.get_teams()
     return db_teams
 
 
@@ -51,13 +62,18 @@ def patch_team(team_id: int, team_update: TeamUpdate, session: Session):
     return team_to_updt.update_team(team_id=team_id, team_update=team_update)
 
 
+def patch_stadium(stadium_id: int, stadium_update: StadiumUpdate, session: Session):
+    stdm_to_updt = StadiumRepository(session=session)
+    return stdm_to_updt.update_stadium(stadium_id=stadium_id, stadium_update=stadium_update)
+
+
 def create_new_team(name: str, titles: int, region:str, session: Session):
     new_team = TeamRepository(session=session)
     new_team.create_team(name=name, titles=titles, region=region)
 
 
 def create_new_stadium(name: str, location: str, team_id: int, session: Session):
-    team = TeamRepository(session=session).get_team_by_id(teamid=team_id)
+    team = TeamRepository(session=session).get_team_by_id(team_id=team_id)
     if not team:
         raise TeamNotFoundError
 
